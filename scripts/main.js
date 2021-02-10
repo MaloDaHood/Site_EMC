5be0de46a84f406a4b0db8e2efbeca4e1e00242c
MaloDaHood <malomr2002@gmail.com>
2021-02-10 16:02:05 +0100

    plus pratique

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-console.log("test");
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+        $('.overlay').toggleClass('active')
+        $('.menu').toggleClass('active')
+    })
+})
