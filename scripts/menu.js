1441162863644382b71967323c9c94d83885bbe7
MaloDaHood <malomr2002@gmail.com>
2021-02-14 13:55:26 +0100

    test script IOS

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.toggle').click(function(){
         $('.toggle').toggleClass('active')
-        $('.overlay').toggleClass('active')
         $('.menu').toggleClass('active')
+        $('.overlay').toggleClass('active')
+        
         alert("OK");
     })
 })
